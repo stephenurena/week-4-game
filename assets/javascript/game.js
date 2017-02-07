2c39bf4d1e4858ea10d9e379dcd57cc73d9085e6
Stephen Urena <surena@Stephens-MacBook-Air.local>
2017-02-06 22:23:01 -0800

    assigned random values from js file to html button tags

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,7 @@ var numberInputs = { //keep in mind, you may have to add more attributes
 	genNumber: "",
 	firstSelection: "",
 	secondSelection: "",
-	
+
 
 	reset: function() {  // keep in mind, you may have to reset more things
 		this.totalScore = "";
@@ -51,34 +51,11 @@ while(arr.length < 4){
     if(arr.indexOf(randomnumber) > -1) continue;
     arr[arr.length] = randomnumber;
 }
-for (var i = 0; i < arr.length; i++) {
-	$("#blueRandom").val(arr[i]);
-	$("#jewelRandom").val(arr[i]);
-	$("#redRandom").val(arr[i]);
-	$("#rubyRandom").val(arr[i]);
-	console.log(arr[i]);
-}
+$(".numberBtn").each(function (i) {
+	$(this).val(arr[i]);
+})
 
 
-// $("#blueRandom").text(jquery.inArray(arr));
-// $("#jewelRandom").val(arr);
-	// // img random number
-	// 	var ImgMinNumber = 20;
-	// 	var ImgMaxNumber = 1;
-		
-	// 	var ImgRandomNumber = randomNumberFromRange(ImgMinNumber, ImgMaxNumber);
-	// 		function randomNumberFromRange(min, max){
-	// 			return Math.floor(Math.random()*(max-min+1)+min);
-				
-	// 		}
-	// 		// $("#blueRandom").val(ImgRandomNumber);
-	// 		// $("#jewelRandom").val(ImgRandomNumber);
-	// 		// $("#redRandom").val(ImgRandomNumber);
-	// 		// $("#rubyRandom").val(ImgRandomNumber);
-	// 		console.log(ImgRandomNumber);
-	// 		var randomArr = Array.from({length: 4}, () => Math.floor(Math.random() * 9));
-	// 		console.log(randomArr);
-		
 
 //Numbers
 	$("button").on("click", function() {
